b0062217a578fa19205c7525325864e06bb35d1a
gomathipksrct@gmail.com <gomathipksrct@gmail.com>
2020-02-12 18:21:11 +0530

    Add project files.

---
+++ MVCSample/Controllers/CollegeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSample.Controllers
+{
+    public class CollegeController : Controller
+    {
+        // GET: College
+        public string Index()
+        {
+            return "K.S.Rangasamy College Of Technology";
+        }
+    }
+}
